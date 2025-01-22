f1946f9065529dbb51a8bacfe3ca39fbcc619c6f
Amir Crljenkovic <32700@ma-web.nl>
2025-01-22 13:17:02 +0100

    changed darkmode/lightmode button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import { useTranslation } from "react-i18next";
 
 import LogoDarkMode from "../img/logo-darkmode.png";
 import LogoLightMode from "../img/logo-lightmode.png";
-import YinYangSVG from "../icons/yinyang.svg";
 import NLFlag from "../img/flags/nl.png";
 import ENFlag from "../img/flags/en.png";
 import DEFlag from "../img/flags/de.png";
@@ -24,7 +23,6 @@ const Header = () => {
     en: { label: "EN", flag: ENFlag },
     de: { label: "DE", flag: DEFlag },
   };
-
 
   useEffect(() => {
     const handleScroll = () => {
@@ -117,6 +115,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full bg-[#F5F5F5] dark:bg-[#202120] shadow-md z-50">
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
+        
         <a href="/" onClick={handleLogoClick} className="flex-shrink-0">
           <img
             src={LogoLightMode}
@@ -130,23 +129,7 @@ const Header = () => {
           />
         </a>
 
-        <div className="md:hidden absolute left-1/2 transform -translate-x-1/2">
-          <div
-            className="relative w-12 h-12 cursor-pointer flex items-center justify-center"
-            onClick={toggleDarkMode}
-          >
-            <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-md">
-              <img
-                src={YinYangSVG}
-                alt="Yin Yang Icon"
-                className={`w-14 h-14 transition-transform duration-500 ease-in-out ${
-                  isDarkMode ? "rotate-180" : "rotate-0"
-                }`}
-              />
-            </div>
-          </div>
-        </div>
-
+        
         <nav className="hidden md:flex flex-1 justify-center space-x-8">
           {navItems.map((item) => (
             <button
@@ -171,22 +154,95 @@ const Header = () => {
           ))}
         </nav>
 
+        
         <div className="hidden md:flex items-center space-x-4">
-          <div
-            className="relative w-12 h-12 cursor-pointer flex items-center justify-center"
-            onClick={toggleDarkMode}
-          >
-            <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-md">
-              <img
-                src={YinYangSVG}
-                alt="Yin Yang Icon"
-                className={`w-14 h-14 transition-transform duration-500 ease-in-out ${
-                  isDarkMode ? "rotate-180" : "rotate-0"
-                }`}
-              />
-            </div>
+          
+          <div className="relative">
+            <input
+              id="darkModeToggle"
+              type="checkbox"
+              checked={isDarkMode}
+              onChange={toggleDarkMode}
+              className="hidden"
+            />
+            <label
+              htmlFor="darkModeToggle"
+              className={`
+                w-[60px] h-[30px] rounded-full
+                shadow-[inset_2px_2px_6px_rgba(0,0,0,0.05)]
+                cursor-pointer
+                relative
+                flex
+                items-center
+                justify-start
+                transition-all
+                duration-500
+                ${
+                  isDarkMode
+                    ? 
+                      "bg-[#B3BFB8] shadow-[inset_2px_2px_6px_rgba(0,0,0,0.5)]"
+                    : 
+                      "bg-[#ECECEC]"
+                }
+              `}
+            >
+              
+              <div
+                className={`
+                  w-[26px] h-[26px] rounded-full 
+                  bg-white 
+                  shadow-[2px_2px_4px_rgba(0,0,0,0.1),-1px_-1px_4px_rgba(0,0,0,0.05)_inset]
+                  absolute 
+                  top-1/2
+                  -translate-y-1/2
+                  flex
+                  items-center
+                  justify-center
+                  overflow-hidden
+                  transition-all
+                  duration-500
+                  ${
+                    isDarkMode
+                      ? "left-[30px] bg-[#2C2C2F] shadow-[2px_2px_6px_rgba(0,0,0,0.5),-2px_-2px_6px_rgba(0,0,0,0.5)_inset]"
+                      : "left-[2px]"
+                  }
+                `}
+              >
+                
+                <svg
+                  className={`sun w-4 absolute text-[#FFD600] transition-all duration-500 ${
+                    isDarkMode
+                      ? "opacity-100 translate-y-0"
+                      : "opacity-0 translate-y-[1rem]"
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+                </svg>
+                
+                <svg
+                  className={`moon w-4 absolute text-[#3C493F] transition-all duration-500 ${
+                    isDarkMode
+                      ? "opacity-0 -translate-y-[1rem]"
+                      : "opacity-100 translate-y-0"
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </label>
           </div>
 
+          
           <div className="relative">
             <button
               className="flex items-center space-x-2"
@@ -231,6 +287,7 @@ const Header = () => {
           </div>
         </div>
 
+        
         <button
           onClick={toggleMenu}
           className="md:hidden flex flex-col items-center justify-center space-y-1"
@@ -253,6 +310,7 @@ const Header = () => {
         </button>
       </div>
 
+      
       <div
         className={`
           md:hidden fixed top-0 left-0 w-full h-screen 
@@ -293,6 +351,89 @@ const Header = () => {
             </button>
           ))}
 
+          
+          <div className="relative">
+            <input
+              id="darkModeToggleMobile"
+              type="checkbox"
+              checked={isDarkMode}
+              onChange={toggleDarkMode}
+              className="hidden"
+            />
+            <label
+              htmlFor="darkModeToggleMobile"
+              className={`
+                w-[60px] h-[30px] rounded-full
+                shadow-[inset_2px_2px_6px_rgba(0,0,0,0.05)]
+                cursor-pointer
+                relative
+                flex
+                items-center
+                justify-start
+                transition-all
+                duration-500
+                ${
+                  isDarkMode
+                    ? "bg-[#B3BFB8] shadow-[inset_2px_2px_6px_rgba(0,0,0,0.5)]"
+                    : "bg-[#ECECEC]"
+                }
+              `}
+            >
+              <div
+                className={`
+                  w-[26px] h-[26px] rounded-full 
+                  bg-white 
+                  shadow-[2px_2px_4px_rgba(0,0,0,0.1),-1px_-1px_4px_rgba(0,0,0,0.05)_inset]
+                  absolute 
+                  top-1/2
+                  -translate-y-1/2
+                  flex
+                  items-center
+                  justify-center
+                  overflow-hidden
+                  transition-all
+                  duration-500
+                  ${
+                    isDarkMode
+                      ? "left-[30px] bg-[#2C2C2F] shadow-[2px_2px_6px_rgba(0,0,0,0.5),-2px_-2px_6px_rgba(0,0,0,0.5)_inset]"
+                      : "left-[2px]"
+                  }
+                `}
+              >
+                
+                <svg
+                  className={`sun w-4 absolute text-[#FFD600] transition-all duration-500 ${
+                    isDarkMode
+                      ? "opacity-100 translate-y-0"
+                      : "opacity-0 translate-y-[1rem]"
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+                </svg>
+                <svg
+                  className={`moon w-4 absolute text-[#3C493F] transition-all duration-500 ${
+                    isDarkMode
+                      ? "opacity-0 -translate-y-[1rem]"
+                      : "opacity-100 translate-y-0"
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </label>
+          </div>
+
+          
           <div className="flex flex-col items-center space-y-2">
             {Object.entries(languages).map(([key, { label, flag }]) => (
               <button
